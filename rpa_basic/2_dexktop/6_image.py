f1bc2a224b300544f355a09281fbbd5366626b95
jung ji min <jimin4017@naver.com>
2021-04-05 23:23:22 +0900

    04.04

---
+++ rpa_basic/2_dexktop/6_image.py
@@ -0,0 +1,20 @@
+import pyautogui
+
+# file_menu = pyautogui.locateOnScreen("menu.png")
+# print(file_menu)
+# pyautogui.click(file_menu)
+
+
+# # 출력문 Box(left=659, top=191, width=50, height=22)
+# # Ctrl win alt space
+
+# trash_icon =pyautogui.locateOnScreen("trash.png")
+# print(trash_icon)
+# pyautogui.moveTo(trash_icon)
+
+# 1. gray scle  
+# trash_icon = pyautogui.locateOnScreen("trash.png",grayscale=True)  ### grayscale = 이미지를 흑백으로 만들어 속도 개선 조금 빨라지나 정확도 낮아짐
+# pyautogui.moveTo(trash_icon)
+
+
+#2. 범위 선정
