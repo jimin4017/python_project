6684ad14e33e9e3637043a394f784998e0799dd5
jung ji min <jimin4017@naver.com>
2021-04-06 23:31:07 +0900

    04.06

--- rpa_basic/2_dexktop/6_image.py
+++ rpa_basic/2_dexktop/6_image.py
@@ -16,5 +16,23 @@ import pyautogui
 # trash_icon = pyautogui.locateOnScreen("trash.png",grayscale=True)  ### grayscale = 이미지를 흑백으로 만들어 속도 개선 조금 빨라지나 정확도 낮아짐
 # pyautogui.moveTo(trash_icon)
 
+ 
+2. 범위 선정  속도 개빨라짐 개굿굿
 
-#2. 범위 선정
+trash_icon = pyautogui.locateOnScreen("trash.png",region=(1802,820,1861-1802,880-820)) 
+pyautogui.moveTo(trash_icon)jimi
+
+pyautogui.mouseInfo()
+위치 확인용
+
+3. 정확
+
+
+
+pyautogui.moveTo(trash_icon) 
+  1828,847 204,204,204 #CCCCCC
+
+
+좌표 얻어 두기
+1802,820 30,30,30 #1E1E1E
+1861,880 30,30,30 #1E1E1E
